10b85545d8e0903fd174cdee6f60e36df206d96f
Gaara <sabakugaara@users.noreply.github.com>
2018-01-19 23:09:53 +0800

    basic demo

--- README.md
+++ README.md
@@ -10,9 +10,44 @@ $ npm install --save debounce-redis
 
 ## Usage
 
+#### Example 1:
+
 ```js
-var debounceRedis = require('debounce-redis');
-debounceRedis();
+var Debounce = require('debounce-redis');
+var debounce = new Debounce({
+  // connect redis options, will pass to ioredis directly, detail see https://github.com/luin/ioredis#connect-to-redis
+  host: '127.0.0.1',
+  port: 6379,
+});
+
+var debounced = debounce(function update() {
+  // do something...
+});
+debounced();
+debounced();
+debounced();
+debounced();  // only update once
+```
+
+#### Example 2:
+
+```js
+var Debounce = require('debounce-redis');
+var debounce = new Debounce({
+  host: '127.0.0.1',
+  port: 6379,
+});
+
+var debounced = debounce(function update() {
+  // do something...
+}, function eventId(id) {
+  return id; 
+});
+
+debounced(1); // event one 
+debounced(1); // event one will only emit once
+debounced(2); // event two
+debounced(2); // event two while emit once too
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 'use strict';
 
-module.exports = function() {
+const Redis = require('ioredis');
+
+const memory = require('./lib/adapter/memory');
+const redis = require('./lib/adapter/redis');
+// const debounce = require('./lib/debounce');
+
+const ADAPTER_MEMORY = 'memory';
+const ADAPTER_REDIS = 'redis';
+
+/**
+ * @param {Object} options
+ * adapter: ['memory', 'redis']
+ */
+module.exports = function create(options) {
+  options = Object.assign({
+    adapter: ADAPTER_MEMORY,
+    wait: 200,
+  }, options);
+  const adapter = options.adapter;
+
+  switch(adapter) {
+    case ADAPTER_MEMORY:
+      return function (func, wait) {
+        wait = wait || options.wait;
+        return memory(func, wait);
+      }
+    case ADAPTER_REDIS:
+      const client = new Redis(options.adapterOptions);
+      return function (func, wait, getEventId) {
+        wait = wait || options.wait;
+        return redis(client, func, wait, getEventId);
+      }
+    default:
+      throw new Error('please select a adapter.');
+  }
 };
